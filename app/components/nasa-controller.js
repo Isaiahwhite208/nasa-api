dd65eed78636223935339be92ee02bd2a7e09ee3
Isaiah White <isaiahwhite208@gmail.com>
2018-08-14 09:49:18 -0600

    push

--- app/components/nasa-controller.js
+++ app/components/nasa-controller.js
@@ -1,5 +1,34 @@
 import NasaService from "./nasa-service.js"
 
-export default class NasaController {
+const nasaService = new NasaService
+let app = document.getElementById('app')
 
+function draw(data) {
+ console.log(data)
+ app.innerHTML = `
+  <div id="error"></div>
+  <button onclick="app.controllers.nasa.getNasa()"> Get Nasa </button>
+  </div>`
 }
+
+function drawNasa(nasa) {
+ let nasaElem = document.getElementById('nasa')
+ let template = ''
+ nasa.results.forEach(nasa => {
+  template += `<div>
+    ${nasa.name}
+    </div>`
+ })
+ nasaElem.innerHTML = template
+}
+
+
+export default class NasaController {
+ constructor() {
+  draw()
+ }
+
+ getNasa() {
+  NasaService.getNasa(console.log, drawError)
+
+ }
--- app/components/nasa-service.js
+++ app/components/nasa-service.js
@@ -2,4 +2,19 @@ import Nasa from "../models/Nasa.js"
 
 export default class NasaService {
 
+ getNasa(draw, drawError) {
+  fetch('https://api.nasa.gov/planetary/apod?api_key=sacGlNHmyRIDXS4x3JD0m5VfNKvRxZ0XZvkDO1Gx')
+   .then(res => res.json())
+   .then(res => {
+    let myNasa = res.results.map(rawNasa => {
+     let nasa = new Nasa(rawNasa)
+     nasa[nasa.id] = nasa
+     return nasa
+    })
+    draw(myNasa)
+   })
+   .catch(drawError)
+ }
+
+
 }
