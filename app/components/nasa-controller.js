37194e05ba5328906e852183ae5a96b649db3791
Isaiah White <isaiahwhite208@gmail.com>
2018-08-14 10:24:43 -0600

    push

--- app/components/nasa-controller.js
+++ app/components/nasa-controller.js
@@ -4,33 +4,37 @@ const nasaService = new NasaService
 let app = document.getElementById('app')
 
 function draw(data) {
- console.log(data)
- app.innerHTML = `
+  console.log(data)
+  app.innerHTML = `
   <div id="error"></div>
   <button onclick="app.controllers.nasa.getNasa()"> Get Nasa </button>
   </div>`
 }
 
 function drawNasa(nasa) {
- let nasaElem = document.getElementById('nasa')
- let template = ''
- nasa.results.forEach(nasa => {
-  template +=
-   `<div>
+  let nasaElem = document.getElementById('nasa')
+  let template = ''
+  nasa.results.forEach(nasa => {
+    template +=
+      `<div>
     ${nasa.name}
     </div>`
- })
- nasaElem.innerHTML = template
+  })
+  nasaElem.innerHTML = template
 }
 
+function drawError(error) {
+  console.log(error)
+  document.getElementById('error').innerHTML = error.message
+}
 
 export default class NasaController {
- constructor() {
-  draw()
- }
+  constructor() {
+    draw()
+  }
 
- getNasa() {
-  NasaService.getNasa(console.log, drawError)
+  getNasa() {
+    nasaService.getNasa(console.log, drawError)
 
- }
+  }
 }
